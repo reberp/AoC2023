b661bbffdd4b75bdc7f546ad55d041171c31dc73
reberp <patrickreber@yahoo.com>
2023-12-14 14:47:43 -0600

    14_2

--- 14/solve.py
+++ 14/solve.py
@@ -8,11 +8,15 @@ sys.path.append('../')
 from utils import common
 
 
-input_file = "test_input.txt"
+input_file = "input.txt"
 
 
 """
-
+For part two:
+* move it north -> same as P1
+* move it west -> switch axis back to normal and move left as before
+* move it south -> flip axis as with p1 and then don't reverse after the sort
+* move it eash -> switch axis back to normal and move without reverse 
 """
 
 
@@ -26,21 +30,27 @@ def parse_file(filename):
 Rotating the contents so that instead of rolling up, I'm rolling left on a single array
 """	
 def rotate_contents(contents):
-	return common.pivot_contents(contents)
+	return common.switch_axis(contents)
 
-def roll_contents_left(contents: list[str]) -> list[str]:
-	print(contents)
+def roll_contents_left(contents: list[str],right=False) -> list[str]:
+	#print(contents)
 	sep = "#"
 	final_contents=[]
 	for row in contents:
 		split_rocks = row.split("#")
 		#sorted does the opposite direction I want but it works to just reverse it
-		sorted_split_rocks = ["".join(sorted(rock)[::-1]) for rock in split_rocks]
+		if right:
+			sorted_split_rocks = ["".join(sorted(rock)) for rock in split_rocks]
+		else:
+			sorted_split_rocks = ["".join(sorted(rock)[::-1]) for rock in split_rocks]
 		temp = '#'.join(sorted_split_rocks)
 		final_sorted_row=temp
 		final_contents.append(final_sorted_row)
-	print(final_contents)
+	#print(final_contents)
 	return final_contents
+
+def roll_contents_right(contents):
+	return roll_contents_left(contents,True)
 
 def calculate_total(rolled_contents):
 	sum=0
@@ -52,12 +62,39 @@ def calculate_total(rolled_contents):
 
 
 contents = parse_file(input_file)
-rotated_contents = rotate_contents(contents)
+
 
 #part 1
-rolled_contents = roll_contents_left(rotated_contents)
-total = calculate_total(rolled_contents)
-print(total)
+#rotated_contents = rotate_contents(contents)
+#rolled_contents = roll_contents_left(rotated_contents)
+#total = calculate_total(rolled_contents)
+#print(total)
+
+#part 2
+rolled_east=contents
+total=0
+myset=set()
+for i in range(1000):
+	rotated_contents = rotate_contents(rolled_east)
+	rolled_north = roll_contents_left(rotated_contents)
+
+	rotated_contents = rotate_contents(rolled_north)
+	rolled_west = roll_contents_left(rotated_contents)
+	
+	rotated_contents = rotate_contents(rolled_west)
+	rolled_south = roll_contents_right(rotated_contents)
+	
+	rotated_contents = rotate_contents(rolled_south)
+	rolled_east = roll_contents_right(rotated_contents)
+	
+	#there's a few numbers and they stabilize into pattern after awhile. 
+	#could probably do some math, or the set is small enough I can guess
+	# would have to figure out a start point for the pattern and it's length 
+	if i>600:
+		myset.add(calculate_total(rotate_contents(rolled_east)))
+	if i>800:
+		print(myset)
+		break
 
 
 
--- utils/common.py
+++ utils/common.py
@@ -6,8 +6,26 @@ def read_file(path):
 		content.append(line.rstrip("\n"))
 	return content	
 
-def pivot_contents(contents):
+def switch_axis(contents):
 	temp=[]
 	pivoted_contents = [pair for pair in zip(*contents)]
 	pivoted_contents_str = ["".join(col) for col in pivoted_contents]
 	return pivoted_contents_str
+
+def rotate_contents_left(contents):
+	return switch_axis(contents)[::-1]
+
+"""
+1 2 3
+4 5 6
+7 8 9
+
+rotate left = pivot_contents(contents)[::-1]
+
+3 6 9
+2 5 8
+1 4 7
+
+rotate left
+
+"""
