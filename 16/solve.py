075bbd7a3566d58c5df4947c7cf3e86e84a996dc
reberp <patrickreber@yahoo.com>
2023-12-17 14:53:06 -0600

    16_1
    works, but not conducive to part 2

--- 16/solve.py
+++ 16/solve.py
@@ -8,13 +8,14 @@ sys.path.append('../')
 from utils import common
 from time import sleep
 
-input_file = "test_input.txt"
+input_file = "input.txt"
 
 
 """
 
 """
-
+# [(location),direction][...]
+all_steps=[] #store direction and location to remove redundancy
 energized=set() #going to hold all the items that are engergized 
 
 
@@ -58,14 +59,23 @@ handle_direction = \
 
 energized=set()
 
-def energize(location):
+def energize(contents,location,print_now=False):
 	energized.add(location)
+	if print_now:
+		for row_num,row in enumerate(contents):
+			for col_num,col in (enumerate(row)):
+				if (col_num==location[1] and row_num==location[0]):
+					print("#",end="")
+				else:
+					print(col,end="")
+			print()
 
-def move(contents,location,direction):
+
+def move(contents,location,direction,start=False):
 	#returns a set of the next move, 
 	#  so that move((0,1),right) returns (0,1),[up,down]
 	next_row,next_col=0,0
-	energize((location[0],location[1]))
+	energize(contents,(location[0],location[1]))
 	#print("Moving: "+str(direction)+" from "+str(location))
 	if direction==["up"]:
 		next_row = location[0]-1
@@ -79,6 +89,9 @@ def move(contents,location,direction):
 	elif direction == ["right"]:
 		next_row= location[0]
 		next_col = location[1]+1
+	if start:
+		next_row=0
+		next_col=0
 	#going below or above
 	if next_row<0 or next_row>=(len(contents)):
 		return (-1,-1),""
@@ -87,35 +100,37 @@ def move(contents,location,direction):
 		return (-1,-1),""			
 	next_loc_value = contents[next_row][next_col]
 	next_direction = handle_direction[direction[0]][next_loc_value]
-	print("From "+str(location)+" "+str(direction)+" to "+str((next_row,next_col))+\
-		" item: "+str(next_loc_value)+" next dir "+str(next_direction))
-	energize((next_row,next_col))
-	print(sorted(energized))
-	print(len(energized))
-	sleep(.1)
+	#print("From "+str(location)+" "+str(direction)+" to "+str((next_row,next_col))+\
+	#	" item: "+str(next_loc_value)+" next dir "+str(next_direction))
+	#energize(contents,(next_row,next_col))
+
+	all_steps.append((location,direction))
 	return ((next_row,next_col),next_direction)
 
 
-def new_laser(contents,loc,direction):
-	print("New laser: "+str(direction))
+def new_laser(contents,loc,direction,start=False):
+	#print("New laser: from {} going {}".format(loc,str(direction)))
 	while direction:
 		#print(loc,direction)
-		next_loc,next_moves = move(contents,loc,direction)	
-		#print(next_loc,next_moves)
+		#print(all_steps)
+		if ((loc,direction) in all_steps) and loc!=(0,0): 
+			#for part 2 have to change for start location? Would that break calculating different direction? 
+			#print("ALREADY DONE THIS")
+			return (-1,-1),""		
+		next_loc,next_moves = move(contents,loc,direction,start)	
 		if next_loc[0]<0:
 			direction=False
-			print("ENDING")
+			#print("End a chain")
 			break
 		if len(next_moves)>1:
-			print("Split")
-			new_laser(contents,next_loc,[next_moves[1]])
+			new_laser(contents,next_loc,[next_moves[1]],start)
 		direction = [next_moves[0]]
 		loc=next_loc		
-		#print(energized)
-		#print(len(energized))
+		start=False 
 
 contents = parse_file(input_file)
+#for part two have to either iterate though all options or [whatever the better answer is]
 loc=(0,0)
 direc=["right"]
-new_laser(contents,loc,direc)
-
+new_laser(contents,loc,direc,start=True)
+print(len(energized))
